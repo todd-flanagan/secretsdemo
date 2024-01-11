44692cf40dfdb1dd0c9240dbb2d29883aa6a0afa
Todd Flanagan <tflanaga@mathworks.com>
2024-01-11 14:58:05 -0500

    failing secret access

--- test/tuseasecret.m
+++ test/tuseasecret.m
@@ -9,6 +9,7 @@ classdef tuseasecret < matlab.unittest.TestCase
 
         function valueTest(testCase)
             testCase.verifyEqual("secretValue", useasecret());
+            testCase.verifyEqual(fakeGetSecret("isasecret"), "testvale");
         end
     end
 
