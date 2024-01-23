99f8356aeff30accd3f61c843c2059893d81f573
tflanaga <tflanaga@ah-tflanaga-m>
2024-01-23 13:59:50 -0500

    added clean task

--- buildfile.m
+++ buildfile.m
@@ -4,7 +4,7 @@ plan = buildplan(localfunctions);
 plan("test") = matlab.buildtool.tasks.TestTask("test", SourceFiles="src");
 plan.DefaultTasks = "test";
 
-
+plan("clean") = matlab.buildtool.tasks.CleanTask();
 
 end
 
